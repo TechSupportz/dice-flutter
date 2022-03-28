d4eb28c9c93d331cda882d7afd2d0ddbca2ca6e6
Nitish <tnitish654@gmail.com>
2022-03-28 23:03:06 +0800

    added dice and button to roll dice (button doesnt do anything)

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,12 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue[100],
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: const Text('Dicee'),
+          backgroundColor: Colors.blue[500],
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     ),
   );
@@ -20,6 +20,37 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Image.asset('images/dice1.png'),
+                  ),
+                  const SizedBox(
+                    width: 24,
+                  ),
+                  Expanded(
+                    child: Image.asset('images/dice1.png'),
+                  ),
+                ],
+              ),
+            ),
+            flex: 2,
+          ),
+          ElevatedButton(
+            onPressed: () => debugPrint('click'),
+            child: const Text('Click me to roll these dice'),
+          ),
+        ],
+      ),
+    );
   }
 }
